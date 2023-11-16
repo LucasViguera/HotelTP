097cf9b4e4274e02d809d8cf6291cb0e846aa3e5
AlvaroR <129621113+AlvaroRutn@users.noreply.github.com>
2023-11-15 23:48:27 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ReservaComponent } from './reserva/reserva.component';
+import { HabitacionesComponent } from './habitaciones/habitaciones.component';
+import { UbicacionComponent } from './ubicacion/ubicacion.component';
+
+//otros
+//import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ConsultaComponent } from './consulta/consulta.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ReservaComponent,
+    HabitacionesComponent,
+    UbicacionComponent,
+    NavbarComponent,
+    FooterComponent,
+    ServiciosComponent,
+    ConsultaComponent
+  ],
+  imports: [
+   // RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserModule,
+    BrowserAnimationsModule, // Importa el módulo de animaciones
+    MatDatepickerModule, // Agrega MatDatepickerModule
+    MatInputModule, // Agrega MatInputModule
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
