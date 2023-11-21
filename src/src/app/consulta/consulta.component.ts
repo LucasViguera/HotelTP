8b762d4969546fc2ec0663024ccae27256301bfa
Lucas Viguera <123002129+LucasViguera@users.noreply.github.com>
2023-11-21 13:35:44 -0300

    Add files via upload

---
+++ src/src/app/consulta/consulta.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-consulta',
+  templateUrl: './consulta.component.html',
+  styleUrls: ['./consulta.component.css']
+})
+export class ConsultaComponent {
+
+}
