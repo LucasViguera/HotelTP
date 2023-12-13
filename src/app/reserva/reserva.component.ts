8df2b19005e1b5306fb58f684dc007290898910a
AlvaroR <129621113+AlvaroRutn@users.noreply.github.com>
2023-12-13 13:08:11 -0300

    New SRC
    -Se creo la funcion enviarformulario en el typescript de reservas.       
    -Se creo la funcion generarPass en el typescript de reservas
    -Se modifico el html y css de consulta (Falta ponerlo mejor es para tener lo basico ya)
    -Se realizaron las validaciones de la funcion enviarFormulario para no enviar datos erroneos y de los datos en el formulario para mostrar el mensaje cuando tengan errores y que el campo no este unicamente "tocado".
    -El logo del navbar ahora permite redirigirte al home 
    -La contraseña de la reserva se muestra luego de finalizar la reserva exitosamente para simplificar
    -Se agrego a la reserva el campo cantidad de personas
    -Se creo la funcion validarCantidadPersonas para validar que la cantidad de los mismos siempre sea entre 1 y 4.

--- src/app/reserva/reserva.component.ts
+++ src/app/reserva/reserva.component.ts
@@ -1,6 +1,7 @@
 import { Component,ViewChild } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
 import { MatDateRangePicker } from '@angular/material/datepicker';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reserva',
@@ -11,8 +12,16 @@ import { MatDateRangePicker } from '@angular/material/datepicker';
 export class ReservaComponent {
   formulario: FormGroup;
   exito: boolean = false;
+  error: boolean = false;
+  tipoHabitacion: string = '';
 
-  // Decorador ViewChild para acceder al elemento con la variable local 'picker'
+
+  //variables del metodo generarPass
+  numeros = '123456789';
+  letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  todo = this.numeros + this.letras;
+
+  /*// Decorador ViewChild para acceder al elemento con la variable local 'picker'
   @ViewChild('picker') dateRangePicker!: MatDateRangePicker<Date>;
   
   // Crea instancias de FormControl para las fechas de inicio y fin
@@ -20,25 +29,60 @@ export class ReservaComponent {
     endDateControl = new FormControl();
 
   // Propiedad para almacenar el rango de fechas seleccionado
-  selectedDateRange: { start: Date | null, end: Date | null } = { start: null, end: null };
+  selectedDateRange: { start: Date | null, end: Date | null } = { start: null, end: null };   No usado porq no se termino usando el tema de fechas todavia*/ 
+
+
   constructor() {
     this.formulario = new FormGroup({
       nombre: new FormControl('', [Validators.required, Validators.minLength(3)]), // Nombre del campo y valor inicial
       email: new FormControl('',[Validators.required, Validators.email]),
-      // Agrega más campos aquí si es necesario
+      direccion: new FormControl(''),
+      telefono: new FormControl('',[Validators.required, Validators.maxLength(9)]),
+      tipohabitacion: new FormControl(''),
+      password: new FormControl(''),
+      cantidadPersonas: new FormControl('', [Validators.required, (control) => this.validarCantidadPersonas(control)])
     });
   }
-  enviarFormulario() {
-    if (this.formulario.valid) {
-      // Aquí puedes realizar acciones de envío y, si es exitoso, mostrar el mensaje de éxito
-      this.exito = true;
-    } else {
-      // Si hay errores de validación, no muestres el mensaje de éxito
-      this.exito = false;
-    }
-  }
+
+        
+        validarCantidadPersonas(control: AbstractControl) {
+          if (control instanceof FormControl) {
+            const cantidad = control.value;
+        
+            if (isNaN(cantidad) || cantidad < 1 || cantidad > 4) {
+              return { cantidadInvalida: true };
+            }
+          }
+        
+          return null;
+        }
   
+
+        //Generar contraseñas para recuperar la informacion de la reserva en consulta  
+        generarPass=(longitud = 10)=>{
+          let password = '';
+          for (let x=0; x<longitud; x++){
+          let aleatorio = Math.floor(Math.random() * this.todo.length);
+          password += this.todo.charAt(aleatorio);
+          }
+          return password;
+        };
+
+        enviarFormulario() {
+
+          if (this.formulario.valid) {
+          //La llamada al generador va aca porque sino solo genera una contraseña por cada vez que se refresca la pagina
+          this.formulario.get('password')?.setValue(this.generarPass());
+
+            const datos = this.formulario.value;
+            console.log('Datos del formulario:', datos);
+            this.error= false;
+            this.exito = true;
+            
+      } else {
+        console.log('Formulario no válido. Por favor, corrija los errores.'); //Es para mostrar en consola que llega hasta este punto
+        Object.values(this.formulario.controls).forEach(control => control.markAsTouched()); //Marca todos los input como tocados por si se desea enviar sin datos
+        this.error= true;
+      }
 }
-
-
-
+}
